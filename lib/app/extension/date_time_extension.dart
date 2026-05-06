49e298fb308fdfd099dee473339235331ae51dcd
xaistrying <xaistrying@gmail.com>
2026-05-06 17:46:42 +0700

    fix: date format

--- lib/app/extension/date_time_extension.dart
+++ lib/app/extension/date_time_extension.dart
@@ -5,6 +5,6 @@ extension DateTimeExtension on DateTime {
   String get uniqueId => microsecondsSinceEpoch.toString();
 
   String toTimeString() {
-    return DateFormat('yyyy/MM/dd HH:mm').format(this);
+    return DateFormat('dd/MM/yyyy HH:mm').format(this);
   }
 }
